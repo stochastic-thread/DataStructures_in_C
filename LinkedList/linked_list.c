46c77a4817413e563cdc9022605f30cba7ef71ab
Arthur Colle <hello@arthurcolle.com>
2021-07-12 00:35:54 -0400

    Can't drop {} if multiline or if semicolon seperator exists

--- LinkedList/linked_list.c
+++ LinkedList/linked_list.c
@@ -23,9 +23,8 @@ void add_to_list(List *list, int item) {
         list->root = create_node(item);
     else {
         temp = list->root;
-        while(temp->next != NULL) {
+        while(temp->next != NULL)
             temp = temp->next;
-        }
         temp->next = create_node(item);
     }
 }
@@ -35,7 +34,8 @@ void display_list(List *list) {
     if(!list->root)
         return;    
     while(temp != NULL) {
-        printf("%d ", temp->value); temp = temp->next;
+        printf("%d ", temp->value); 
+        temp = temp->next;
     }
     newline();
 }
